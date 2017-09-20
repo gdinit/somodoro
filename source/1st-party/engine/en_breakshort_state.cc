a8e27bd646629f15eba6ded3b3b93ce90d325c89
gdinit <gdinit@gmx.com>
2017-09-20 18:22:22 +0100

    add en_breakshort_state

---
+++ source/1st-party/engine/en_breakshort_state.cc
@@ -0,0 +1,494 @@
+/* en_breakshort_state.cc */
+// ===================================80 chars=================================|
+
+#include "en_breakshort_state.h"
+
+// TODO delete these?
+extern std::unique_ptr <Settings>	SETTINGS;
+extern std::unique_ptr <Globals>	GLOBALS;
+
+// Used to increase/decrease window size with hotkeys
+#define SIZE_STEP_PX 20
+#define MIN_SIZE_PX 40
+#define FONT_SIZE_PX 74
+
+BreakshortState::BreakshortState( StateMachine &machine
+	, sf::RenderWindow &window
+	, EngineSharedContext &context
+	, bool replace )
+	: State{ machine, window, context, replace }
+	, m_myObjNameStr( "BreakshortState" )
+{
+	loadSounds();
+	playSoundIfRequested();
+	// Reset to prevent instant-game-over next time
+	GLOBALS->returnToMainMenuRequested = 0;
+	m_timerLive = true;
+
+	// TODO: remove initializeState() - move all into ctor
+	initializeState();
+
+	// must happen after everything else
+	winSizeDecrease( 3 );
+	m_enSharedContext.winMMMustResize = !m_enSharedContext.winMMMustResize;
+}
+
+BreakshortState::~BreakshortState()
+{
+	#if defined DBG
+	std::cout << "[DEBUG]\tDestructed state:\t" << m_myObjNameStr << "\n";
+	#endif
+
+	// TODO remove me
+	#if defined DBG
+	std::cout << "[DEBUG]\tCountdownState run time was: " <<
+	getStateAgeAsSeconds() << " seconds\n";
+	#endif
+}
+
+void BreakshortState::initializeState()
+{
+	#if defined DBG
+	std::cout << "[DEBUG]\tCreated state:\t\t" << m_myObjNameStr << "\n";
+	#endif
+
+	restartStateClock();
+	// TODO base these values on config variables
+	m_desiredAspectRatio = 640.f / 480.f;
+	#if defined DBG
+	std::cout << "[DEBUG]\tm_desiredAspectRatio is: \t" <<
+	m_desiredAspectRatio << " //" << m_myObjNameStr << "\n";
+	#endif
+
+	// need this? m_worldView = m_window.getDefaultView();
+	m_urgentUpdateNeeded = 10;
+
+	// debug overlay font
+	m_font.loadFromFile( "assets/fonts/sansation.ttf" );
+	m_statisticsText.setFont( m_font );
+	m_statisticsText.setPosition( 5.f, 5.f );
+	m_statisticsText.setCharacterSize( 12u );
+	m_statisticsText.setFillColor( sf::Color::White );
+	updateDebugOverlayTextIfEnabled( true );
+
+	// countdown font
+	// m_breakshortFont.loadFromFile( "assets/fonts/sansation.ttf" );
+	m_breakshortFont.loadFromFile( "assets/fonts/monofont.ttf" );
+	m_breakshortText.setFont( m_breakshortFont );
+	// centerOrigin( m_breakshortText );
+	// m_breakshortText.setPosition( m_windowSize.x / 2, m_windowSize.y / 2
+	// );
+	// m_breakshortText.setPosition( 5.f, 5.f );
+	m_breakshortText.setCharacterSize( FONT_SIZE_PX );
+	m_breakshortText.setFillColor( sf::Color::White );
+
+	// START A NEW GAME
+	m_windowSize = m_window.getSize();
+	m_res.x = static_cast <float> ( m_windowSize.x );
+	m_res.y = static_cast <float> ( m_windowSize.y );
+
+	////////////////////////////////////////
+	// SFML::ImGui Tests
+	m_deltaClock.restart();
+	ImGui::SFML::Init( m_window );
+	m_clicked = 0;
+	////////////////////////////////////////
+
+	std::ifstream	i( "data/settings.json" );
+	nlohmann::json	j;
+	i >> j;
+	for ( nlohmann::json::iterator it = j.begin(); it != j.end(); ++it ) {
+		if ( it.key() == "m_countdownBgColorR" ) {
+			m_countdownBgColorR = it.value();
+		} else if ( it.key() == "m_countdownBgColorG" ) {
+			m_countdownBgColorG = it.value();
+		} else if ( it.key() == "m_countdownBgColorB" ) {
+			m_countdownBgColorB = it.value();
+		} else if ( it.key() == "m_secsPomodoro" ) {
+			m_secsPomodoro = it.value();
+		} else if ( it.key() == "m_secsBreakShort" ) {
+			m_secsBreakShort = it.value();
+		} else if ( it.key() == "m_secsBreakLong" ) {
+			m_secsBreakLong = it.value();
+		}
+	}
+	i.close();
+	m_breakshortBgColor.r = m_countdownBgColorR;
+	m_breakshortBgColor.g = m_countdownBgColorG;
+	m_breakshortBgColor.b = m_countdownBgColorB;
+	std::cout 
+	<< "Short Break is: " << m_secsBreakShort << " seconds.\n"
+	<< "Long Break is: " << m_secsBreakLong << " seconds.\n"
+	<< "Pomodoro started - counting down: " << m_secsPomodoro << " seconds.\n";
+
+	// TODO change this to steady clock
+	m_TPstart = std::chrono::system_clock::now();
+}
+
+void BreakshortState::update()
+{
+	sf::Time m_elapsedTime = m_clock.restart();
+	m_timeSinceLastUpdate += m_elapsedTime;
+
+	if ( GLOBALS->returnToMainMenuRequested == 1 ) {
+		// Game over! Terminate
+		#if defined DBG
+		std::cout << "Terminating due to returnToMainMenuRequested=1\n";
+		#endif
+		m_next = StateMachine::build <MainMenuState>
+				( m_machine, m_window, m_enSharedContext
+				, true );
+	}
+
+	while ( m_timeSinceLastUpdate > State::TimePerFrame ) {
+		m_timeSinceLastUpdate -= State::TimePerFrame;
+
+		processEvents();
+		// obtain current screen res - need to pass it to game objects
+		m_res = static_cast <sf::Vector2f>
+			( m_window.getSize() );
+		if ( m_enSharedContext.mustMainMenu == true ) {
+			m_next = StateMachine::build <MainMenuState> (
+					m_machine, m_window
+					, m_enSharedContext
+					, true );
+		}
+		// update statistics for the debug overlay
+		m_statisticsUpdateTime += m_elapsedTime;
+		m_statisticsNumFrames += 1;
+		// update statsText only once a second
+		if ( m_urgentUpdateNeeded > 0 ) {
+			// update now!
+			--m_urgentUpdateNeeded;
+			updateDebugOverlayTextIfEnabled();
+			printConsoleDebugIfEnabled();
+			m_urgentUpdateNeeded = false;
+		}
+		if ( m_statisticsUpdateTime >= sf::seconds( 1.0f ) ) {
+			if ( m_statisticsNumFrames <= 1 ) {
+				break;	// if we're playing catchup, don't
+				// bother with debugOverlayText
+			}
+			recordObservedFPS();
+			dynamicallyAdjustFPSLimit();
+			updateDebugOverlayTextIfEnabled();
+			printConsoleDebugIfEnabled();
+			m_statisticsUpdateTime -= sf::seconds( 1.0f );
+			m_statisticsNumFrames = 0;
+		}
+	}
+
+	if ( m_timerLive ) {
+		calculateUpdateTimer();
+	}
+}
+
+void BreakshortState::draw()
+{
+	m_enSharedContext.frameID++;
+	m_window.clear( m_breakshortBgColor );
+	m_grabbedWindow = m_windowActive
+		&& m_enSharedContext.winMoveable
+		&& sf::Mouse::isButtonPressed( sf::Mouse::Left );
+	if ( m_grabbedWindow ) {
+		m_window.setPosition( sf::Mouse::getPosition() +
+			m_grabbedOffset );
+	}
+	m_window.setView( m_enSharedContext.view );
+	if ( SETTINGS->inGameOverlay ) {
+		m_window.draw( m_statisticsText );
+	}
+
+	m_window.draw( m_breakshortText );
+
+	// Display only if PauseState is not doing it.
+	m_window.display();
+}
+
+void BreakshortState::pause()
+{
+	#if defined DBG
+	std::cout << "[DEBUG]\tPaused State:\t\t" << m_myObjNameStr << "\n";
+	#endif
+}
+
+void BreakshortState::resume()
+{
+	restartStateClock();
+	m_urgentUpdateNeeded = 10;
+	// destroy the queue
+	// give me stats in the first frame, but first make up some plausible
+	// values
+	updateDebugOverlayTextIfEnabled( true );
+
+	#if defined DBG
+	std::cout << "[DEBUG]\tResumed State:\t\t" << m_myObjNameStr << "\n";
+	#endif
+
+	// if there is a pending play sound request, play it
+	if ( m_enSharedContext.reqPlaySound ) {
+		m_enSharedContext.reqPlaySound = 0;
+		m_sClicked.play();
+	}
+}
+
+void BreakshortState::processEvents()
+{
+	sf::Event evt;
+	while ( m_window.pollEvent( evt ) ) {
+		ImGui::SFML::ProcessEvent( evt );
+		switch ( evt.type ) {
+		case sf::Event::Closed:
+			m_machine.quit();
+			break;
+		case sf::Event::GainedFocus:
+			m_windowActive = true;
+			break;
+		case sf::Event::LostFocus:
+			m_windowActive = false;
+			makeWindowAlwaysOnTop();
+			break;
+		case sf::Event::Resized:
+			// onResize();
+			m_enSharedContext.view = getLetterboxView(
+					m_enSharedContext.view
+					, evt.size.width
+					, evt.size.height );
+			break;
+		case sf::Event::MouseButtonPressed:
+			if ( evt.mouseButton.button == sf::Mouse::Left ) {
+				m_grabbedOffset = m_window.getPosition() -
+					sf::Mouse::getPosition();
+			}
+			break;
+		case sf::Event::KeyPressed:
+			switch ( evt.key.code ) {
+			case sf::Keyboard::Escape:
+			case sf::Keyboard::Q:
+				std::cout << "Quitting on Q key"
+				" press. Goodbye!\n";
+				m_machine.quit();
+				break;
+			case sf::Keyboard::M:
+				m_next = StateMachine::build
+					<MainMenuState> ( m_machine, m_window
+						, m_enSharedContext, true );
+				break;
+			case sf::Keyboard::F2:
+				this->tglDbgShowOverlay();
+				break;
+			case sf::Keyboard::F3:
+				this->
+				toggleDebugConsoleOutput();
+				break;
+			case sf::Keyboard::F4:
+				this->
+				tglDbgDFPSConsOutput();
+				break;
+			default:
+				break;
+			}
+			break;
+		case sf::Event::KeyReleased:
+			switch ( evt.key.code ) {
+			case sf::Keyboard::Add:
+			case sf::Keyboard::Num9:
+				winSizeIncrease( 1 );
+				break;
+			case sf::Keyboard::Subtract:
+			case sf::Keyboard::Num0:
+				winSizeDecrease( 1 );
+				break;
+			case sf::Keyboard::L:
+				winToggleMoveable();
+				break;
+			default:
+				break;
+			}
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+void BreakshortState::onResize()
+{
+}
+
+void BreakshortState::makeWindowAlwaysOnTop()
+// TODO: move this to state.cc
+{
+	#ifdef _WIN32
+	// define something for Windows (32-bit and 64-bit, this part is common)
+	HWND hwnd = m_window.getSystemHandle();
+	SetWindowPos( hwnd, HWND_TOPMOST, 0, 0, 0, 0
+		, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE );
+
+	#ifdef _WIN64
+	// define something for Windows (64-bit only)
+	#else
+	// define something for Windows (32-bit only)
+	#endif
+	#elif __APPLE__
+	#include "TargetConditionals.h"
+	#if TARGET_IPHONE_SIMULATOR
+	// iOS Simulator
+	#elif TARGET_OS_IPHONE
+	// iOS device
+	#elif TARGET_OS_MAC
+	// Other kinds of Mac OS
+	#else
+	#   error "Unknown Apple platform"
+	#endif
+	#elif __linux__
+	// linux
+	#elif __unix__	// all unices not caught above
+	// Unix
+	#elif defined ( _POSIX_VERSION )
+	// POSIX
+	#else
+	#   error "Unknown compiler"
+	#endif
+}
+
+void BreakshortState::calculateUpdateTimer()
+{
+	m_TPlatest = std::chrono::system_clock::now();
+	std::chrono::duration <double> elapsed_secs = m_TPlatest - m_TPstart;
+	m_countdownSecondsRemaining = m_secsPomodoro - round(
+			elapsed_secs.count() );
+
+	if ( elapsed_secs.count() >= m_secsPomodoro ) {
+		m_timerLive = false;
+	}
+
+	if ( !m_window.hasFocus() ) {
+		std::this_thread::sleep_for( std::chrono::milliseconds( 500 ) );
+	}
+	updateText();
+	centerText();
+}
+
+void BreakshortState::updateText()
+{
+	m_breakshortText.setString( "" );
+	int	minutes = ( m_countdownSecondsRemaining / 60 ) % 60;
+	int	seconds = m_countdownSecondsRemaining %	60;
+	if ( seconds == 0 ) {
+		m_breakshortText.setString(
+			// MINUTES
+			std::to_string( minutes )
+			// SEPARATOR & SECONDS
+			+ ":00"
+			// Close
+			);
+	} else if ( seconds > 0 && seconds <= 9 ) {
+		m_breakshortText.setString(
+			// MINUTES
+			std::to_string( minutes )
+			// SEPARATOR
+			+ ":0"
+			// SECONDS
+			+ std::to_string( seconds )
+			// Close
+			);
+	} else {
+		m_breakshortText.setString(
+			// MINUTES
+			std::to_string( minutes )
+			// SEPARATOR
+			+ ":"
+			// SECONDS
+			+ std::to_string( seconds )
+			// Close
+			);
+	}
+}
+
+void BreakshortState::centerText()
+{
+	if ( m_centerOriginNeeded == true ) {
+		m_centerOriginNeeded = false;
+		centerOrigin( m_breakshortText );
+	}
+	centerOrigin( m_breakshortText );
+	m_breakshortText.setPosition( m_windowSize.x / 2, m_windowSize.y / 2 );
+}
+
+void BreakshortState::loadSounds()
+{
+	if ( !m_sbClicked.loadFromFile(
+		     "assets/sounds/clicked.wav" ) ) {
+	}
+	m_sClicked.setBuffer( m_sbClicked );
+}
+
+void BreakshortState::playSoundIfRequested()
+{
+	if ( m_enSharedContext.reqPlaySound ) {
+		m_enSharedContext.reqPlaySound = 0;
+		playSoundClicked();
+	}
+}
+
+void BreakshortState::playSoundClicked()
+{
+	#if defined DBG
+	std::cout << "[DEBUG] Playing a sound.\t" << m_myObjNameStr << "\n";
+	#endif
+	m_sClicked.play();
+}
+
+void BreakshortState::winSizeIncrease( int times )
+{
+	for ( int n = 0; n < times; n++ ) {
+		sf::Vector2u	cSize = m_window.getSize();
+		sf::Vector2u	nSize
+			= { cSize.x + SIZE_STEP_PX, cSize.y + SIZE_STEP_PX };
+
+		#if defined DBG
+		std::cout << "[DEBUG] curSize: " << cSize.x << "," << cSize.y <<
+		"\tnewSize: " << nSize.x << "," << nSize.y << "\t//" <<
+		m_myObjNameStr << "\n";
+		#endif
+		m_window.setSize( nSize );
+	}
+}
+
+void BreakshortState::winSizeDecrease( int times )
+{
+	for ( int n = 0; n < times; n++ ) {
+		sf::Vector2u	cSize = m_window.getSize();
+		sf::Vector2u	nSize = { cSize.x - SIZE_STEP_PX
+					  , cSize.y - SIZE_STEP_PX };
+		#if defined DBG
+		std::cout << "[DEBUG] curSize: " << cSize.x << "," << cSize.y <<
+		"\tnewSize: " << nSize.x << "," << nSize.y << "\t//" <<
+		m_myObjNameStr << "\n";
+		#endif
+
+		if ( nSize.x > MIN_SIZE_PX && nSize.y > MIN_SIZE_PX ) {
+			m_window.setSize( nSize );
+		}
+	}
+}
+
+void BreakshortState::winToggleMoveable()
+{
+	m_enSharedContext.winMoveable = !m_enSharedContext.winMoveable;
+	#if defined DBG
+	std::string newValueText;
+	if ( m_enSharedContext.winMoveable ) {
+		newValueText = "ON\t";
+	} else {
+		newValueText = "OFF";
+	}
+	std::cout << "[DEBUG] Toggled moveable. New value: " << newValueText <<
+	"\t//" << m_myObjNameStr << "\n";
+	#endif
+}
+
+// ===================================80 chars=================================|
+/* EOF */
+
---
+++ source/1st-party/engine/en_breakshort_state.h
@@ -0,0 +1,119 @@
+/* en_breakshort_state.h */
+// ===================================80 chars=================================|
+
+#ifndef EN_BREAKSHORT_STATE_H
+#define EN_BREAKSHORT_STATE_H
+
+#include "en_essentials.h"
+#include "en_state.h"
+#include "en_state_machine.h"
+#include "en_main_menu_state.h"
+// Required for centerOrigin
+#include "en_util_utility.h"
+#include "en_util_make_unique.h"
+
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/System/Time.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/Text.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Audio.hpp>
+#include <imgui.h>
+#include <imgui-SFML.h>
+
+// to in-line document
+// Required for std::unique_ptr
+#include <memory>
+// Required for debugging -- TODO REMOVE THIS
+#include <iostream>
+// Required for capturing last windowResizeEvent
+#include <chrono>
+// Required for ceil
+#include <cmath>
+
+#include <ctime>
+#include <ratio>
+#include <chrono>
+
+// Required for JSON
+#include "3rd-party/json-nlohmann/v.2.1.1/json.hpp"
+// Required for JSON
+#include <fstream>
+// To sleep & reduce CPU usage
+#include <thread>
+
+class StateMachine;
+
+class BreakshortState : public State
+{
+public:
+	BreakshortState( StateMachine &machine
+	, sf::RenderWindow &window
+	, EngineSharedContext &context
+	, bool replace = true );
+	virtual ~BreakshortState();
+	void	onResize();
+	void	initializeState();
+	void	processEvents();
+	void	update();
+	void	draw();
+	void	pause();
+	void	resume();
+	void	makeWindowAlwaysOnTop();
+	void	calculateUpdateTimer();
+	void	updateText();
+	void	centerText();
+	void	loadSounds();
+	void	playSoundIfRequested();
+	void	playSoundClicked();
+	void	winSizeIncrease( int times );
+	void	winSizeDecrease( int times );
+	void	winToggleMoveable();
+
+private:
+	const t_objectName	m_myObjNameStr;
+	sf::Vector2f		m_res;
+
+	sf::Vector2u		m_textureSize;
+	sf::Vector2u		m_windowSize;
+	// TODO move this to app
+	float
+				m_desiredAspectRatio;
+	// imgui stuff
+	sf::Clock		m_deltaClock;
+	int32			m_clicked;
+	// pomodoro stuff
+	sf::Vector2i		m_grabbedOffset;
+	bool			m_grabbedWindow = false;
+	bool			m_windowActive = true;
+	// This should be overwritten by JSON parser
+	sf::Color		m_breakshortBgColor = sf::Color::Yellow;
+	int32			m_countdownBgColorR = 0;
+	int32			m_countdownBgColorG = 0;
+	int32			m_countdownBgColorB = 0;
+	int32			m_secsPomodoro = 0;
+	int32			m_secsBreakShort = 0;	
+	int32			m_secsBreakLong = 0;	
+	int32			m_countdownSecondsRemaining = 0;
+
+	sf::Font		m_breakshortFont;
+	sf::Text		m_breakshortText;
+
+	stdTimePoint		m_TPstart;
+	stdTimePoint		m_TPlatest;
+	bool			m_centerOriginNeeded = true;
+	bool			m_timerLive = false;
+	// SOUNDS
+	sf::SoundBuffer		m_sbClicked;
+	sf::Sound		m_sClicked;
+};
+
+// EN_BREAKSHORT_STATE_H
+#endif
+
+// ===================================80 chars=================================|
+/* EOF */
+
